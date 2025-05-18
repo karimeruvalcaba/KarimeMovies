1d31542c7063d3e7151579f717132db3914e28e7
Karime Itzel Ruvalcaba Pérez <kruvalcabaperez@gmail.com>
2025-05-18 17:45:31 -0600

    vercel redirectick link

--- src/components/LoginWithTMDbButton/LoginWithTMDbButton.tsx
+++ src/components/LoginWithTMDbButton/LoginWithTMDbButton.tsx
@@ -1,23 +1,30 @@
-"use client"
+"use client";
 import { getRequestToken } from "@/services/movies/getRequestToken";
 
 const LoginWithTMDbButton = () => {
-    const handleLogin = async () =>{
-        try{
-            const token = await getRequestToken();
-            window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/auth/callback`;
-        }catch(err){
-            console.error("Failed to get TMBb token", err);
-        }
-    };
+  const handleLogin = async () => {
+    try {
+      const token = await getRequestToken();
 
-    return (
-        <button
-            onClick={handleLogin}
-            className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
-        >
-            Login using TMDb
-        </button>
-    )
-}
+      const redirectTo =
+        process.env.NEXT_PUBLIC_TMDB_REDIRECT_URL || "http://localhost:3000/auth/callback";
+
+      window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${encodeURIComponent(
+        redirectTo
+      )}`;
+    } catch (err) {
+      console.error("Failed to get TMDb token", err);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleLogin}
+      className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
+    >
+      Login using TMDb
+    </button>
+  );
+};
+
 export default LoginWithTMDbButton;
