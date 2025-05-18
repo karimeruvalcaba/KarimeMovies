1fdc08be203d372fb4475333c817c1f3610e152b
Karime Itzel Ruvalcaba Pérez <kruvalcabaperez@gmail.com>
2025-05-18 17:20:21 -0600

    this should make the deployment work pt2

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,37 +1,42 @@
-"use client"
-import { useEffect } from "react";
+'use client';
+
+import { Suspense, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import api from "@/services/api";
 
-const CallbackPage = () => {
-    
-    const params = useSearchParams();
-    const router = useRouter();
+function CallbackHandler() {
+  const params = useSearchParams();
+  const router = useRouter();
 
-    useEffect(() => {
-        const fetchSession = async () =>{
-            const request_token = params.get("request_token");
-            if (!request_token) return;
+  useEffect(() => {
+    const fetchSession = async () => {
+      const request_token = params.get("request_token");
+      if (!request_token) return;
 
-            const res = await api.post("/authentication/session/new", {
-                request_token,
-            });
+      const res = await api.post("/authentication/session/new", {
+        request_token,
+      });
 
-            const sessionId = res.data.session_id;
-            localStorage.setItem("tmdb_session_id", sessionId);
+      const sessionId = res.data.session_id;
+      localStorage.setItem("tmdb_session_id", sessionId);
 
-            // Fetch account ID
-            const accRes = await api.get(`/account?session_id=${sessionId}`);
-            localStorage.setItem("tmdb_account_id", accRes.data.id);
+      // Fetch account ID
+      const accRes = await api.get(`/account?session_id=${sessionId}`);
+      localStorage.setItem("tmdb_account_id", accRes.data.id);
 
-            router.push("/my-favorites");
+      router.push("/my-favorites");
+    };
 
-        };
+    fetchSession();
+  }, [params, router]);
 
-        fetchSession();
-    }, [params, router]);
+  return <p className="p-6">Authorizing with TMDb...</p>;
+}
 
-    return <p className="p-6"> Authorizing with TMBb...</p>
-};
-
-export default CallbackPage;
+export default function CallbackPage() {
+  return (
+    <Suspense fallback={<p className="p-6">Loading...</p>}>
+      <CallbackHandler />
+    </Suspense>
+  );
+}
